970eb27365c48563818104ed8cc0e2ae7cc9af83
ranibs <shrani@gmail.com>
2021-11-02 12:49:17 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+	
+
+	ngOnInit() {
+
+
+
+	}
+
 }
+
+
+
+  // counter=0;
+  // classtoset='positive';
+  // onCounterChange(event:string){
+  //  event=='INC'?this.counter++:this.counter--;
+  //  this.classtoset=this.counter>=0?'positive':'negative';
+  // }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutUsComponent } from './homepage/about-us/about-us.component';
+import { ContactusComponent } from './homepage/contactus/contactus/contactus.component';
+import { SigninComponent } from './homepage/signin/signin.component';
+import { SignupComponent } from './homepage/signup/signup.component';
+
+// import { UserComponent } from './user/user.component';
+// import { HelloComponent } from './hello/hello.component';
+// import { ProductComponent } from './product/product.component';
+// import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    AboutUsComponent,
+    ContactusComponent,
+    SigninComponent,
+    SignupComponent
+    // UserComponent,
+    // HelloComponent,
+    // ProductComponent,
+    // LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
